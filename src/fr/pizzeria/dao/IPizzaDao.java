fbd36bf6a6311f404d57da3f17445c5a44db41b2
thien ban BUI <thienban91@hotmail.fr>
2017-12-07 10:42:36 +0100

    SQL server

--- src/fr/pizzeria/dao/IPizzaDao.java
+++ src/fr/pizzeria/dao/IPizzaDao.java
@@ -1,14 +1,11 @@
 package fr.pizzeria.dao;
-import java.util.ArrayList;
 import java.util.List;
-
-import java.util.Scanner;
 
 import fr.pizzeria.console.Pizza;
 
 public interface IPizzaDao {
 
-	List<Pizza> pizzas findAllPizzas();
+	List<Pizza> findAllPizzas();
 
 	boolean saveNewPizza(Pizza pizza);
 
--- src/fr/pizzeria/dao/PizzaDaoTableau.java
+++ src/fr/pizzeria/dao/PizzaDaoTableau.java
@@ -3,8 +3,6 @@ package fr.pizzeria.dao;
 import java.util.ArrayList;
 
 import java.util.List;
-import java.util.Scanner;
-
 import fr.pizzeria.console.Pizza;
 
 public class PizzaDaoTableau implements IPizzaDao {
